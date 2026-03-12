b16b2ddd907b39b5411698ddd004dc6ba21dc6db
shiwangi-upadhyay <shiwangiupadhyay332@gmail.com>
2026-03-12 11:45:28 +0530

    add RadialOrbitalTimeline component for an interactive radial visualization of timeline data with expandable details and related item highlighting.

--- src/components/ui/radial-orbital-timeline.tsx
+++ src/components/ui/radial-orbital-timeline.tsx
@@ -135,7 +135,7 @@ export default function RadialOrbitalTimeline({ timelineData }: RadialOrbitalTim
             return (
               <div
                 key={item.id}
-                ref={(el) => (nodeRefs.current[item.id] = el)}
+                ref={(el) => { nodeRefs.current[item.id] = el; }}
                 className="absolute transition-all duration-700 cursor-pointer"
                 style={{
                   transform: `translate(${pos.x}px, ${pos.y}px)`,
